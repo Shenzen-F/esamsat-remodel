7f48c4f6239c9c8e3e5d0bd3d37bf0d90aef8946
Shenzen-F <230705044@student.ar-raniry.ac.id>
2026-08-31 11:24:13 +0700

    Penambaahan Feature Heatsatat Google analytics (undone(butuh url real))

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+
+    allowedHosts: true,
+
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443
+    }
+  }
 })
