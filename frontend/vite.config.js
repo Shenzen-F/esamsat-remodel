809b9893cd4f932fb2f4a64c11d3f4058e577463
Shenzen-F <230705044@student.ar-raniry.ac.id>
2026-09-14 15:05:12 +0700

    feat: add GitHub Actions workflow for frontend deployment and Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/esamsat-remodel/',
+
   plugins: [vue()],
+
   server: {
     host: true,
-
     allowedHosts: true
   }
 })
